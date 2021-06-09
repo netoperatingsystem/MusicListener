604468891995dcffe9be099231b72c4dc5a7b4a1
Maciej Sochalski <maciejsochalski@gmail.com>
2021-06-09 19:33:51 +0200

    merge

--- MusicListener/app/src/main/java/com/example/musiclistener/MainActivity.java
+++ MusicListener/app/src/main/java/com/example/musiclistener/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.musiclistener;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -94,6 +96,17 @@ public class MainActivity extends AppCompatActivity {
 
         customAdapter customAdapter = new customAdapter();
         listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String songName = (String) listView.getItemAtPosition(i);
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+                        .putExtra("songs", mySongs)
+                        .putExtra("songname", songName)
+                        .putExtra("pos", i));
+            }
+        });
     }
 
     class customAdapter extends BaseAdapter
